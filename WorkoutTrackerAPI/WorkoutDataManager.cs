c827af61665cd48d877fd98d14a644ee90c16273
Saffi Khawaja <saffikhawaja@gmail.com>
2026-03-17 12:25:32 +0500

    added save data functionality across workout manager and workoutdatamanager.

--- WorkoutTrackerAPI/WorkoutDataManager.cs
+++ WorkoutTrackerAPI/WorkoutDataManager.cs
@@ -54,7 +54,6 @@ public class workoutdatamanager
                 }
             }
         }
-
     }
 
     public void SaveData(WorkoutManagerService workoutManagerService)
--- WorkoutTrackerAPI/WorkoutManager.cs
+++ WorkoutTrackerAPI/WorkoutManager.cs
@@ -2,21 +2,28 @@ namespace WorkoutTrackerAPI;
 
 public class WorkoutManagerService
 {
+    workoutdatamanager workoutDataManager = new workoutdatamanager(this);
     //i want to read data from the file using workoutdatamanager class and save it to a list of workouts
     private List<Workout> workouts = new List<Workout>();
 
     public WorkoutManagerService()
     {
         //this constructor is going to load the data from the textfile when the application is started.
-        //we need to create an instance of the workoutdatamanager class and pass the workout manager service to it, so it can load the data from the textfile to the list of workouts
-        workoutdatamanager workoutDataManager = new workoutdatamanager(this);
-
+        workoutDataManager.ReadData(this);
     }
 
     public void AddWorkout(Workout workout)
     {
-        workout.Id = workouts.Count + 1; // Assign a unique ID based on the current count of workouts
+        //first check if ID is already in use, if it is, we need to assign a new ID to the workout
+        if (workouts.Any(w => w.Id == workout.Id))
+        {
+            workout.Id = workouts.Count + 1; // Assign a unique ID based on the current count of workouts
+        }
+
         workouts.Add(workout);
+
+        //i also need to call SaveData method of the workoutdatamanager class to save the data to the textfile when a new workout is added
+        workoutDataManager.SaveData(this);
     }
 
     public List<Workout> GetWorkouts()
@@ -32,11 +39,17 @@ public class WorkoutManagerService
     public void DeleteAllWorkouts()
     {
         workouts.Clear();
+
+        //i also need to call SaveData method of the workoutdatamanager class to save the data to the textfile when all workouts are deleted
+        workoutDataManager.SaveData(this);
     }
 
     public void DeletebyIDWorkout(int id)
     {
         workouts.RemoveAll(w => w.Id == id);
+        //i also need to call SaveData method of the workoutdatamanager class to save the data to the textfile when a workout is deleted
+        workoutDataManager.SaveData(this);
+
     }
 
     public void UpdateWorkout(int id, Workout workout)
@@ -46,6 +59,9 @@ public class WorkoutManagerService
         {
             workouts[index] = workout;
         }
+
+        //i also need to call SaveData method of the workoutdatamanager class to save the data to the textfile when a workout is updated
+        workoutDataManager.SaveData(this);
     }
 
 }
